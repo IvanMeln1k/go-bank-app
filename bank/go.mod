6d8e801a7b6e8abb0c0c934bf9e84f334ebfb56b
Ivan Melnik <IvanMelnikV@ya.ru>
2024-04-06 10:47:33 +0300

    postgres,redis,hasher,tokens

--- bank/go.mod
+++ bank/go.mod
@@ -4,13 +4,18 @@ go 1.22.1
 
 require (
 	github.com/getkin/kin-openapi v0.123.0
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/oapi-codegen/runtime v1.1.1
+	github.com/redis/go-redis/v9 v9.5.1
 	github.com/sirupsen/logrus v1.8.1
 )
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.8 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
@@ -22,12 +27,14 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.32.0 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ bank/pkg/tokens/tokens.go
@@ -0,0 +1,132 @@
+package tokens
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var (
+	ErrTokenExpired = errors.New("token is expired")
+	ErrTokenInvalid = errors.New("token is invalid")
+)
+
+type TokenManagerInterface interface {
+	CreateRefreshToken() (string, error)
+	CreateAccessToken(userId int) (string, error)
+	CreateEmailToken(email int) (string, error)
+	ParseAccessToken(tokenString string) (int, error)
+	ParseEmailToken(tokenString string) (string, error)
+}
+
+type TokenManager struct {
+	SecretKey string
+	AccessTTL time.Duration
+	EmailTTL  time.Duration
+}
+
+func (tm *TokenManager) CreateRefreshToken() (string, error) {
+	b := make([]byte, 32)
+
+	src := rand.NewSource(time.Now().Unix())
+	r := rand.New(src)
+
+	if _, err := r.Read(b); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", b), nil
+}
+
+type ClaimsAccessToken struct {
+	jwt.StandardClaims
+	Id int `json:"id"`
+}
+
+type ClaimsEmailToken struct {
+	jwt.StandardClaims
+	Email string `json:"email"`
+}
+
+func (tm *TokenManager) createStandartClaims(ttl time.Duration) jwt.StandardClaims {
+	return jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(ttl).Unix(),
+		IssuedAt:  time.Now().Unix(),
+	}
+}
+
+func (tm *TokenManager) createJWTToken(claims jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(tm.SecretKey))
+}
+
+func (tm *TokenManager) CreateAccessToken(userId int) (string, error) {
+	return tm.createJWTToken(ClaimsAccessToken{
+		tm.createStandartClaims(tm.AccessTTL),
+		userId,
+	})
+}
+
+func (tm *TokenManager) CreateEmailToken(email string) (string, error) {
+	return tm.createJWTToken(ClaimsEmailToken{
+		tm.createStandartClaims(tm.EmailTTL),
+		email,
+	})
+}
+
+func (tm *TokenManager) ParseAccessToken(tokenString string) (int, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, ErrTokenInvalid
+		}
+		claims, ok := t.Claims.(*ClaimsAccessToken)
+		if !ok {
+			return nil, ErrTokenInvalid
+		}
+		if claims.ExpiresAt <= time.Now().Unix() {
+			return nil, ErrTokenExpired
+		}
+		return []byte(tm.SecretKey), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*ClaimsAccessToken)
+	if !ok {
+		return 0, ErrTokenInvalid
+	}
+
+	return claims.Id, nil
+}
+
+func (tm *TokenManager) ParseEmailToken(tokenString string) (string, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, ErrTokenInvalid
+		}
+		claims, ok := t.Claims.(*ClaimsEmailToken)
+		if !ok {
+			return nil, ErrTokenInvalid
+		}
+		if claims.ExpiresAt <= time.Now().Unix() {
+			return nil, ErrTokenExpired
+		}
+		return []byte(tm.SecretKey), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(*ClaimsEmailToken)
+	if !ok {
+		return "", ErrTokenInvalid
+	}
+
+	return claims.Email, nil
+}
